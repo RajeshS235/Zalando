14f1db98f3e8cbb03c6c138347a42c8a273aae26
RajeshS235 <rajeshsv.1987@gmail.com>
2020-11-27 12:50:31 +0530

    all files commit

---
+++ src/test/java/org/spi/LoginPage.java
@@ -0,0 +1,14 @@
+package org.spi;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginPage {
+public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\admin\\eclipse-workspace\\com.maven\\Driver\\chromedriver.exe");
+
+	WebDriver driver = new ChromeDriver();
+
+	driver.get("https://www.amazon.in/");
+}
+}
